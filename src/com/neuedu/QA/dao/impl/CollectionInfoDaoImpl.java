7d424950f73016554ddbe7d27e67c04edd6ba629
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-21 15:54:49 +0800

    1
    1

--- src/com/neuedu/QA/dao/impl/CollectionInfoDaoImpl.java
+++ src/com/neuedu/QA/dao/impl/CollectionInfoDaoImpl.java
@@ -8,21 +8,33 @@ import com.neuedu.QA.dbutil.BaseDao;
 public class CollectionInfoDaoImpl extends BaseDao implements CollectionInfoDao {
 
 	@Override
-	public ResultSet SelectAllCollection(int user_id) {
+	public ResultSet SelectAllCollection(String user_id) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql_1 = "select question_id from collect where u_id = ?";
+		String sql = "select * from question where question_id in ("+sql_1+")";
+		String params[] = {user_id};
+		return executeSelect(sql, params);
 	}
 
 	@Override
-	public int AddCollection(int user_id, int question_id) {
+	public int AddCollection(String user_id, int question_id) {
 		// TODO Auto-generated method stub
-		return 0;
+		String sql = "insert into collect (u_id,question_id) values(?,?)";
+		String params[] = {user_id , question_id+""};
+		return executeIUD(sql, params);
 	}
 
 	@Override
-	public int DeleteCollection(int user_id, int question_id) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int DeleteCollection(String user_id, int question_id) {
+		// TODO Auto-generated method stub//更新数据库不通过主键，mysql会报错，需要把set——safe——update设置为0才能执行
+		String set_safe_0 = "set sql_safe_updates = 0;";
+		//sql语句
+		String sql = "delete from collect where u_id = ? and question_id = ?;";
+		//恢复安全设置
+		String set_safe_1 = "set sql_safe_updates = 1;";
+				
+		String params[] = {user_id , question_id+""};
+		return executeIUD(set_safe_0+sql+set_safe_1, params);
 	}
 
 }
--- src/com/neuedu/QA/dao/impl/UserFollowDaoImpl.java
+++ src/com/neuedu/QA/dao/impl/UserFollowDaoImpl.java
@@ -6,23 +6,36 @@ import com.neuedu.QA.dao.UserFollowDao;
 import com.neuedu.QA.dbutil.BaseDao;
 
 public class UserFollowDaoImpl extends BaseDao implements UserFollowDao {
-
+	
 	@Override
-	public ResultSet SelectAllFollow(int user_id) {
+	public ResultSet SelectAllFollow(String user_id) {
 		// TODO Auto-generated method stub
-		return null;
+		String sql_1 = "select follow_u_id from user_follow where u_id = ?";
+		String sql = "select * from user_info where u_id in ("+sql_1+")";
+		String params[] = {user_id};
+		return executeSelect(sql, params);
 	}
 	
 	@Override
-	public int AddFollow(int user_id, int to_user_id) {
+	public int AddFollow(String user_id, String to_user_id) {
 		// TODO Auto-generated method stub
-		return 0;
+		String sql = "insert into user_follow(u_id,follow_u_id) values(?,?)";
+		String params[] = {user_id , to_user_id};
+		return executeIUD(sql, params);
 	}
 
 	@Override
-	public int DeleteFollow(int user_id, int to_user_id) {
+	public int DeleteFollow(String user_id, String to_user_id) {
 		// TODO Auto-generated method stub
-		return 0;
+		//更新数据库不通过主键，mysql会报错，需要把set——safe——update设置为0才能执行
+		String set_safe_0 = "set sql_safe_updates = 0;";
+		//sql语句
+		String sql = "delete from user_follow where u_id = ? and follow_u_id = ?;";
+		//恢复安全设置
+		String set_safe_1 = "set sql_safe_updates = 1;";
+				
+		String params[] = {user_id , to_user_id};
+		return executeIUD(set_safe_0+sql+set_safe_1, params);
 	}
 
 }
