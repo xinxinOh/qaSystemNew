7bd2ddeb9ee0b5e5cbb45bd079e7c39d6fb79627
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-21 10:08:57 +0800

    Update AnswerServiceImpl.java

--- src/com/neuedu/QA/service/impl/AnswerServiceImpl.java
+++ src/com/neuedu/QA/service/impl/AnswerServiceImpl.java
@@ -1,6 +1,42 @@
 package com.neuedu.QA.service.impl;
 
-public class AnswerServiceImpl {
-	
-}
+import java.util.ArrayList;
 
+import com.neuedu.QA.dao.impl.AnswerDaoImpl;
+import com.neuedu.QA.entity.Answer;
+import com.neuedu.QA.service.AnswerService;
+
+public class AnswerServiceImpl implements AnswerService {
+	
+	private AnswerDaoImpl answerDao;
+	
+	public AnswerServiceImpl() {
+		// TODO Auto-generated constructor stub
+		answerDao = new AnswerDaoImpl();
+	}
+
+	@Override
+	public int addAnswer(Answer answer, int question_id, int user_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteAnswer(int answer_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateAnswer(Answer answer) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Answer> showUserAnswer(int user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
