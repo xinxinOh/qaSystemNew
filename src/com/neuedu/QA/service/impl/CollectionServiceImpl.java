d57a9434d9a6ba7f4bde93db5e54c7a1ff691af9
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-21 10:01:41 +0800

    Update CollectionServiceImpl.java

--- src/com/neuedu/QA/service/impl/CollectionServiceImpl.java
+++ src/com/neuedu/QA/service/impl/CollectionServiceImpl.java
@@ -2,12 +2,19 @@ package com.neuedu.QA.service.impl;
 
 import java.util.ArrayList;
 
-import com.neuedu.QA.dbutil.BaseDao;
+import com.neuedu.QA.dao.impl.CollectionInfoDaoImpl;
 import com.neuedu.QA.entity.Collect;
 import com.neuedu.QA.service.CollectionService;
 
 public class CollectionServiceImpl implements CollectionService {
 
+	private CollectionInfoDaoImpl collectionInfo;
+	
+	public CollectionServiceImpl() {
+		// TODO Auto-generated constructor stub
+		collectionInfo = new CollectionInfoDaoImpl();
+	}
+	
 	@Override
 	public Integer addCollection(int user_id, int question_id) {
 		// TODO Auto-generated method stub
