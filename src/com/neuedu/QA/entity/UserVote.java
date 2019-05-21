17d96938bf305cc3a095128a5f915f8d39555ead
zzjNiubi <50816473+zzjNiubi@users.noreply.github.com>
2019-05-21 16:12:28 +0800

    UserVote Integet

--- src/com/neuedu/QA/entity/UserVote.java
+++ src/com/neuedu/QA/entity/UserVote.java
@@ -2,9 +2,9 @@ package com.neuedu.QA.entity;
 
 public class UserVote {
 	private String user_id;
-	private int vote_to_id;
-	private int vote_type;
-	private int category;
+	private Integer vote_to_id;
+	private Integer vote_type;
+	private Integer category;
 	
 	public String getUser_id() {
 		return user_id;
@@ -12,22 +12,22 @@ public class UserVote {
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-	public int getVote_to_id() {
+	public Integer getVote_to_id() {
 		return vote_to_id;
 	}
-	public void setVote_to_id(int vote_to_id) {
+	public void setVote_to_id(Integer vote_to_id) {
 		this.vote_to_id = vote_to_id;
 	}
-	public int getVote_type() {
+	public Integer getVote_type() {
 		return vote_type;
 	}
-	public void setVote_type(int vote_type) {
+	public void setVote_type(Integer vote_type) {
 		this.vote_type = vote_type;
 	}
-	public int getCategory() {
+	public Integer getCategory() {
 		return category;
 	}
-	public void setCategory(int category) {
+	public void setCategory(Integer category) {
 		this.category = category;
 	}
 }
