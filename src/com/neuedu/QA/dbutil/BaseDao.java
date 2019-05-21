ca702fb7d66a52bb7d877bcb6dc6b9fe15e12bd9
xinxinOh <479530479@qq.com>
2019-05-21 15:15:22 +0800

    ;;

--- src/com/neuedu/QA/dbutil/BaseDao.java
+++ src/com/neuedu/QA/dbutil/BaseDao.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 /**
- * ������
+ * 锟斤拷锟斤拷锟斤拷
  * @author Administrator
  *
  */
@@ -15,24 +15,24 @@ public class BaseDao {
 	private String driver="com.mysql.jdbc.Driver";
 	private String url="jdbc:mysql://localhost:3306/qasys";
 	private String user="root";
-	private String pwd="123456";
+	private String pwd="ddd123";
 	public static Connection con=null;
 	public static PreparedStatement pst =null;
 	public static ResultSet rs = null;
 	
 	/**
-	 * ��ȡ�Ự�ķ���
+	 * 锟斤拷取锟结话锟侥凤拷锟斤拷
 	 * @return
 	 */
 	public Connection getCon(){
 		try {
-			Class.forName(driver);//����mysql����
+			Class.forName(driver);//加载mysql驱动
 			con=DriverManager.getConnection(url,user,pwd);
 		} catch (ClassNotFoundException e) {
-			System.out.println("��������ʧ��");
+			System.out.println("驱动加载失败");
 			e.printStackTrace();
 		} catch (SQLException e) {
-			System.out.println("���ӽ���ʧ��");
+			System.out.println("连接建立失败");
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -42,7 +42,7 @@ public class BaseDao {
 	}
 	
 	/**
-	 * ���õĹرո���jdbc����ķ���
+	 * 锟斤拷锟矫的关闭革拷锟斤拷jdbc锟斤拷锟斤拷姆锟斤拷锟�
 	 * @param con
 	 * @param st
 	 * @param rs
@@ -59,31 +59,31 @@ public class BaseDao {
 				con.close();
 			}
 		} catch (Exception e2) {
-			System.out.println("�ر����ݿ�ʧ��");
+			System.out.println("锟截憋拷锟斤拷锟捷匡拷失锟斤拷");
 			e2.printStackTrace();
 		}
 		
 	}
 	
 	/**
-	 * ����ɾ�ĵĲ�����ȡ�ɹ�ͨ�ķ���
-	 * @return int ��ʾӰ�������
+	 * 锟斤拷锟斤拷删锟侥的诧拷锟斤拷锟斤拷取锟缴癸拷通锟侥凤拷锟斤拷
+	 * @return int 锟斤拷示影锟斤拷锟斤拷锟斤拷锟�
 	 */
 	
 	public int executeIUD(String sql , Object[] params){
-		PreparedStatement pst =null;//Ԥ����sql������
-		con = getCon();//ͨ����ͨ�����õ����ݿ�����
-		int count =0;//����Ӱ�������ı���
+		PreparedStatement pst =null;//预锟斤拷锟斤拷sql锟斤拷锟斤拷锟斤拷
+		con = getCon();//通锟斤拷锟斤拷通锟斤拷锟斤拷锟矫碉拷锟斤拷锟捷匡拷锟斤拷锟斤拷
+		int count =0;//锟斤拷锟斤拷影锟斤拷锟斤拷锟斤拷锟侥憋拷锟斤拷
 		try {
 			pst=con.prepareStatement(sql);
-			//ƴ�Ӳ���  ,һ��?��Ӧһ������
+			//拼锟接诧拷锟斤拷  ,一锟斤拷?锟斤拷应一锟斤拷锟斤拷锟斤拷
 			if (null!=params) {
 				for (int i = 0; i <params.length; i++) {
-					pst.setObject(i+1, params[i]);//Ϊsql�е�ÿһ��?��ֵ
+					pst.setObject(i+1, params[i]);//为sql锟叫碉拷每一锟斤拷?锟斤拷值
 				}
 			}
 			
-			count=pst.executeUpdate();//PreparedStatement����ɾ�ķ���
+			count=pst.executeUpdate();//PreparedStatement锟斤拷锟斤拷删锟侥凤拷锟斤拷
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -95,18 +95,18 @@ public class BaseDao {
 	}
 	
 	/**
-	 * ��ͨ�Ĳ�ѯ����,����ֵ�ǽ����
+	 * 锟斤拷通锟侥诧拷询锟斤拷锟斤拷,锟斤拷锟斤拷值锟角斤拷锟斤拷锟�
 	 * @param sql
 	 * @param params
 	 * @return
 	 */
 	public ResultSet executeSelect(String sql , Object[] params){
-		con = getCon();//ͨ����ͨ�����õ����ݿ�����
+		con = getCon();//通锟斤拷锟斤拷通锟斤拷锟斤拷锟矫碉拷锟斤拷锟捷匡拷锟斤拷锟斤拷
 		try {
 			pst=con.prepareStatement(sql);
 			if (null!=params) {
 				for (int i = 0; i <params.length; i++) {
-					pst.setObject(i+1, params[i]);//Ϊsql�е�ÿһ��?��ֵ
+					pst.setObject(i+1, params[i]);//为sql锟叫碉拷每一锟斤拷?锟斤拷值
 				}
 			}
 			
@@ -114,7 +114,7 @@ public class BaseDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally{
-			//����ע�ⲻ�ܵ���closeAll����,��Ȼ���ڵò���rs�е�ֵŶ
+			//锟斤拷锟斤拷注锟解不锟杰碉拷锟斤拷closeAll锟斤拷锟斤拷,锟斤拷然锟斤拷锟节得诧拷锟斤拷rs锟叫碉拷值哦
 		}
 		
 		return rs;
