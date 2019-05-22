1f7444461e3a17e8447184af22a889265e8d761f
zzjNiubi <50816473+zzjNiubi@users.noreply.github.com>
2019-05-22 11:30:50 +0800

    Service ZZJ

--- src/com/neuedu/QA/service/impl/LoadAnswerServiceImpl.java
+++ src/com/neuedu/QA/service/impl/LoadAnswerServiceImpl.java
@@ -1,31 +1,53 @@
 package com.neuedu.QA.service.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.neuedu.QA.dao.impl.AnswerDaoImpl;
-import com.neuedu.QA.entity.Question;
+import com.neuedu.QA.entity.Answer;
 import com.neuedu.QA.service.LoadAnswerService;
 
 public class LoadAnswerServiceImpl implements LoadAnswerService{
 
 	public AnswerDaoImpl answerDaoImpl = new AnswerDaoImpl();
 	
+	
+
 	@Override
-	public ArrayList<Question> LoadPopularQuestion() {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Answer> LoadAnswer(int question_id,int start,int end) {
+		
+		ResultSet rs = answerDaoImpl.SelectAnswer(question_id, start, end);
+		ArrayList<Answer> answer_list = new ArrayList<Answer>();
+		try {
+			while (rs.next()) {
+				answer_list.add(new Answer(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6),rs.getInt(7),rs.getDate(8)));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return answer_list;
 	}
 
 	@Override
-	public ArrayList<Question> LoadCategoryQuestion(int type, int start, int end) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ArrayList<Question> LoadUserCenterQuestion(String user_id) {
-		// TODO Auto-generated method stub
-		return null;
+	public ArrayList<Answer> LoadUserCenterAnswer(String user_id) {
+		
+		ResultSet rs = answerDaoImpl.SelectAllAnswer(user_id);
+		ArrayList<Answer> answer_list = new ArrayList<Answer>();
+		try {
+			while (rs.next()) {
+				answer_list.add(new Answer(rs.getInt(1),rs.getInt(2),rs.getString(3),rs.getString(4),rs.getInt(5),rs.getInt(6),rs.getInt(7),rs.getDate(8)));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return answer_list;
 	}
 
 }
