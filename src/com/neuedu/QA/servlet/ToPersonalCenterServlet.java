dd0af3f5e1be0a79edf6eccfbac47ac72812c2e6
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-27 13:57:58 +0800

    1
    1

--- src/com/neuedu/QA/servlet/ToPersonalCenterServlet.java
+++ src/com/neuedu/QA/servlet/ToPersonalCenterServlet.java
@@ -45,51 +45,14 @@ public class ToPersonalCenterServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("ToPersonalCenterServlet");
-		String user_id = ((UserInfo)request.getSession().getAttribute("user")).getUser_id();
-		
-		//设置问题回答
-		AnswerService answerService = new AnswerServiceImpl();
-		ArrayList<Answer> answers = answerService.showUserAnswer(user_id,0,10);
-		request.getSession().setAttribute("answers", answers);
-		
-		//回答的问题
-		AskQuestionService askQuestionService = new AskQuestionServiceImpl();
-		ArrayList<Question> answerQuestions = new ArrayList<Question>();
-		if (answers.size()==0) {
-			Iterator<Answer> answerIt = answers.iterator();
-			while (answerIt.hasNext()) {
-				Answer answer = answerIt.next();
-				int q_id = answer.getQuestion_id();
-				Question question = askQuestionService.selectQuestion(q_id);
-				answerQuestions.add(question);
-			}	
-		}
-		request.getSession().setAttribute("answerQuestions", answerQuestions);
-		
-		//设置收藏问题;
-		CollectionService collectionService = new CollectionServiceImpl();
-		ArrayList<Question> collectQuestions = collectionService.showCollection(user_id, 0, 10);
-		request.getSession().setAttribute("collectQuestions", collectQuestions);
-		
-		System.out.println("sherched "+collectQuestions.size());
-		
-		//设置提问题
-		AskQuestionService questionService = new AskQuestionServiceImpl();
-		ArrayList<Question> questions = questionService.ShowUserQuestion(user_id, 0, 10);
-		request.getSession().setAttribute("questions", questions);
-		System.out.println("sherched "+questions.size());
-		request.getRequestDispatcher("//user_home_page/userHeader.jsp").forward(request, response);
-		
+	request.getRequestDispatcher("//LoadUserContentServlet").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-gene rated method stub
 		doGet(request, response);
 	}
 
