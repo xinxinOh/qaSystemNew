ae7f57281b7500c2e83e6e766f708e473b5c1120
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-30 10:14:46 +0800

    1

---
+++ src/com/neuedu/QA/filter/servletFilter_Login.java
@@ -0,0 +1,57 @@
+package com.neuedu.QA.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class servletFilter_Login
+ */
+@WebFilter(filterName = "/servletFilter_Login" ,urlPatterns = "/ToPersonalCenterServlet")
+public class servletFilter_Login implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public servletFilter_Login() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		if (((HttpServletRequest)request).getSession().getAttribute("user")!= null) {
+			chain.doFilter(request, response);
+			return;
+		}else {
+			request.getRequestDispatcher("//login.jsp").forward(request, response);
+			return;
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
